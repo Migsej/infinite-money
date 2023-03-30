e03462c390dfece30e5bd9f2470b3704976ce255
Migsej <vincentkbonne@gmail.com>
2023-03-30 10:44:11 +0200

    added text :D

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def postname_comment(subreddit, posts):
     postarray = []
     print("getting post name")
     url = f"https://reddit.com/r/{subreddit}/top/.json?limit={posts}"
-    postr = requests.get(url, headers={'Accept': 'application/json'})
+    postr = requests.get(url, headers={'Accept': 'application/json', 'User-agent': 'your bot 0.1'})
     #print(r.json())
 
     postjson = json.loads(postr.text)
@@ -19,7 +19,7 @@ def postname_comment(subreddit, posts):
         title = postjson["data"]["children"][i]["data"]["title"]
         print("finding comment name")
         commenturl = postjson["data"]["children"][i]["data"]["url"]
-        r = requests.get(f"{commenturl}.json?limit=1", headers={'Accept': 'application/json'})
+        r = requests.get(f"{commenturl}.json?limit=1", headers={'Accept': 'application/json', "User-agent": "your bot 0.1"})
         jsonresponse = json.loads(r.text)
         comment = jsonresponse[1]["data"]["children"][0]["data"]["body"]
         #print(comment)
@@ -55,12 +55,22 @@ def main():
     for i, name in enumerate(posts):
         with open("audio.mp3", "wb") as f:
             f.write(text_to_speech(name))
-        input_gameplay = ffmpeg.input("./gameplaynew.mp4")
         input_audio = ffmpeg.input("./audio.mp3")
 
         audiolen = ffmpeg.probe("audio.mp3")['format']['duration']
+        input_gameplay = ffmpeg.input("./gameplaynew.mp4") 
+        listname = name.split(" ")
+        print(audiolen)
+        for j, text in enumerate(listname):
+            wordlen = float(audiolen) / len(listname)
+
+            input_gameplay = input_gameplay.drawtext(text=text,
+                fontfile="./static/SourceCodePro-Medium.ttf",
+                fontcolor="white",x="(w-tw)/2",y="((h-text_h)/2)-(text_h-(th/4))",
+                fontsize="64",enable=f"between(t,{j*wordlen},{(j+1)*wordlen})")
 
         combined = combine_video_audio(input_gameplay.trim(start=0, end=audiolen), input_audio, f"./finished{i}.mp4")
+        
         combined.run()
 
 if __name__ == "__main__":
